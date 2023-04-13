a5fa0eccece287ab32eada956140696b2e576eb0
Daniel Visicatto <dpfreestyle@gmail.com>
2023-04-13 09:23:42 -0300

    Add project files.

---
+++ Formula1/Entities/Driver.cs
@@ -0,0 +1,55 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Formula1.Entities
+{
+    [BsonIgnoreExtraElements]
+    internal class Driver
+    {
+        // O processo abaixo denomina-se serialização. Cada elemento recebido como string do nosso documento será atribuido nestas propriedades
+
+        [BsonId]// Data notation, refere-se a um objeto unico da nossa base de dados dizendo dentro da nossa aplicação, que vai trabalhar com aquele tipo de dados
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Driver")]
+        public string? DriverName { get; set; }
+
+        [BsonElement("Abbreviation")]
+        public string? Abbreviation { get; set; }
+
+        [BsonElement("No")]
+        [BsonRepresentation(BsonType.Int32)]
+        public int Number { get; set; }
+
+        [BsonElement("Team")]
+        public string? Team { get; set; }
+
+        [BsonElement("Coutry")]
+        public string? Country { get; set; }
+
+        [BsonElement("Date of Birth")]
+        public string? BirthDate { get; set; }
+
+        public Driver(string? driverName, string? abbreviation, int number,
+            string? team, string? country, string? birthDate)
+        {
+            DriverName = driverName;
+            Abbreviation = abbreviation;
+            Number = number;
+            Team = team;
+            Country = country;
+            BirthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {DriverName}\n" +
+                   $"Abreviação: {Abbreviation}\n" +
+                   $"Numero do carro: {Number}\n" +
+                   $"Equipe: {Team}\n" +
+                   $"País de Origem: {Country}\n" +
+                   $"Data de Nascimento: {BirthDate}\n\n"; 
+        }
+    }
+}
---
+++ Formula1/Program.cs
@@ -0,0 +1,116 @@
+﻿using Formula1.Entities;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       
+        //Connection Stringo com o nosso banco de dados MongoDB
+        MongoClient mongo = new("mongodb://localhost:27017");
+
+        #region[Testada a conexão]
+        //trazendo a lista de bancos para verificar conexão OK.
+        //var listabancos = mongo.ListDatabaseNames().ToList();
+
+        //foreach (var database in listabancos)
+        //{
+        //    Console.WriteLine(database);
+        //}
+        #endregion
+
+
+        var dataBases = mongo.GetDatabase("Formula1"); // vai guardar minha base de dados
+        var collection = dataBases.GetCollection<BsonDocument>("Pilotos"); // vai buscar nossa coleção de pilotos.
+        var document = collection.Find(new BsonDocument()).ToList(); // gera um obj e recebe o primeiro registro da collection e trazer para ele
+
+
+        #region[CREATE]
+        //Console.WriteLine("Nome:");
+        //string n = Console.ReadLine();
+
+        //Console.WriteLine("Abreviacao:");
+        //string a = Console.ReadLine();
+
+        //Console.WriteLine("Numero:");
+        //int num = int.Parse(Console.ReadLine());
+
+        //Console.WriteLine("Equipe:");
+        //string eq = Console.ReadLine();
+
+        //Console.WriteLine("Pais:");
+        //string p = Console.ReadLine();
+
+        //Console.WriteLine("Data de Nascimento:");
+        //string bd = Console.ReadLine();
+
+
+        //Driver driver = new(n, a, num, eq, p, bd);
+        //Console.WriteLine(driver.ToString());
+
+        ////gerando um novo documento usando os campos do objeto que existia
+        //var dr = new BsonDocument
+        //{
+        //    {"Driver", driver.DriverName},
+        //    {"Abbreviation", driver.Abbreviation},
+        //    {"No", driver.Number},
+        //    {"Team", driver.Team},
+        //    {"Coutry", driver.Country},
+        //    {"Date od Birth", driver.BirthDate},
+        //};
+        //Console.WriteLine(dr);
+        //collection.InsertOne(dr);
+        #endregion
+
+        #region[UPDATE]
+        //Console.WriteLine("Informe o nome do piloto:");
+        //string pilotName = Console.ReadLine();
+
+        //Console.WriteLine("Informe o novo número:");
+        //int numb = int.Parse(Console.ReadLine());
+
+        //var filter = Builders<BsonDocument>.Filter.Regex("Driver", pilotName);
+        //var update = Builders<BsonDocument>.Update.Set("No", numb);        
+
+        //collection.UpdateOne(filter,update);
+        #endregion
+
+        #region[DELETE]
+        //Console.WriteLine("Informe o nome do piloto:");
+        //string pilotName = Console.ReadLine();
+
+        //var filter = Builders<BsonDocument>.Filter.Eq("Driver", pilotName); // Equals porque senão pode apagar um outro
+
+        //collection.FindOneAndDelete(filter);
+        #endregion
+
+
+        #region[COMMENTED]
+        //Console.WriteLine("Informe o nome do piloto: ");
+        //var v = Console.ReadLine();
+
+        //var filter = Builders<BsonDocument>.Filter.Regex("Driver", v); // a classe Builder cria a consulta no Mongo para nós {"Driver": "Sergio Perez"}
+
+        //var pilot = collection.Find(filter).FirstOrDefault();
+        // var driver = BsonSerializer.Deserialize<Driver>(collection.Find(filter).FirstOrDefault());
+
+        //Console.WriteLine(driver.ToString());
+
+        //Console.WriteLine(pilot.ToString());
+
+        //Console.WriteLine("INforme o nome da equipe:");
+        //var e = Console.ReadLine();
+
+        //filter = Builders<BsonDocument>.Filter.Regex("Team", e);
+        //var pilots = collection.Find(filter).ToList();
+
+        //foreach (var driver in pilots)
+        //{
+        //    Console.WriteLine(driver);
+        //}
+        #endregion
+    }
+
+}
